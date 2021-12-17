01f391716fc349afaa032d40c409afc4d0f9fefe
Konstantin0 <maschkovc@yandex.ru>
2021-12-17 21:14:35 +0300

    преобразование к double

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -3,38 +3,38 @@ package model;
 public class Calculator {
 
     public static String execute(String[] params) {
-        if (Integer.parseInt(params[1]) > 100 | Integer.parseInt(params[1]) < 0 |
-                Integer.parseInt(params[2]) > 100 | Integer.parseInt(params[2]) < 0 ) {
+        if (Double.parseDouble(params[1]) > 100 | Double.parseDouble(params[1]) < 0 |
+                Double.parseDouble(params[2]) > 100 | Double.parseDouble(params[2]) < 0 ) {
             System.out.println("Error! Вы вышли за пределы допустимых значений");
             System.out.println(new CalculatorException());
             return "Error!";
         } else {
-            int result = 0;
+            double result = 0;
             switch (params[0]) {
                 case ("+"):
-                    result = Integer.parseInt(params[1]) + Integer.parseInt(params[2]);
+                    result = Double.parseDouble(params[1]) + Double.parseDouble(params[2]);
                     break;
                 case ("-"):
-                    result = Integer.parseInt(params[1]) - Integer.parseInt(params[2]);
+                    result = Double.parseDouble(params[1]) - Double.parseDouble(params[2]);
                     break;
                 case ("*"):
-                    result = Integer.parseInt(params[1]) * Integer.parseInt(params[2]);
+                    result = Double.parseDouble(params[1]) * Double.parseDouble(params[2]);
                     break;
                 case ("/"):
-                    result = Integer.parseInt(params[1]) / Integer.parseInt(params[2]);
+                    result = Double.parseDouble(params[1]) / Double.parseDouble(params[2]);
                     break;
                 case ("%"):
-                    result = Integer.parseInt(params[1]) % Integer.parseInt(params[2]);
+                    result = Double.parseDouble(params[1]) % Double.parseDouble(params[2]);
                     break;
                 case ("^"):
-                    result = (int) Math.pow(Integer.parseInt(params[1]), Integer.parseInt(params[2]));
+                    result = (int) Math.pow(Double.parseDouble(params[1]),Double.parseDouble(params[2]));
                     break;
                 default:
                     new CalculatorException();
                     break;
             }
             System.out.println("Результат вычисления равен " + result);
-            return Integer.toString(result);
+            return (String.valueOf(result));
         }
     }
 
