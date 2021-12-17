d794ec9834426e189c86cbeedf75b734303e0152
Konstantin0 <maschkovc@yandex.ru>
2021-12-17 23:07:25 +0300

    правки калькулятора и тестов 23.07

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -6,8 +6,7 @@ public class Calculator {
         if (Double.parseDouble(params[1]) > 100 | Double.parseDouble(params[1]) < -100 |
                 Double.parseDouble(params[2]) > 100 | Double.parseDouble(params[2]) < -100 ) {
             System.out.println("Error! Вы вышли за пределы допустимых значений");
-            new CalculatorException();
-            return "Error!";
+            throw new CalculatorException();
         } else {
             double result = 0;
             switch (params[0]) {
