ef177cabcfc1174875d569be7532e84878b6e704
Konstantin0 <maschkovc@yandex.ru>
2021-12-18 13:59:25 +0300

    Правки main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println( " Введите операцию ( + , - , * , / ) и число от -100 до 100  формате " +
-                "операция - число - число (например + 1 1). " );
 
         args = new String[3];
         args[0] = in.next();
