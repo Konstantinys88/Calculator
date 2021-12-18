e40fcb2a6d8bfa62ceaa5bcf9e12caccde335064
Konstantin0 <maschkovc@yandex.ru>
2021-12-18 13:54:08 +0300

    Правки main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.println( " Введите операцию ( + , - , * , / ) и число от -100 до 100  формате " +
+                "операция - число - число (например + 1 1). " );
 
-        String[] array = new String[3];
-        args[0] = in.nextLine();
-        args[1] = in.nextLine();
-        args[2] = in.nextLine();
+        args = new String[3];
+        args[0] = in.next();
+        args[1] = in.next();
+        args[2] = in.next();
 
-        Calculator.execute(array);
-
+        Calculator.execute(args);
 
     }
 }
