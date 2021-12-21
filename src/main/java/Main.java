4ba47ef09c399de87e362991b489095893d1c0c6
Konstantin0 <maschkovc@yandex.ru>
2021-12-21 15:29:59 +0300

    Комментарии

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         * Данные в консоль нужно вводить в следующем порядке оператор число, число (+ 1 1).
+         * Калькулятор принимает только числа от -100 до 100.
+         */
+
         Scanner in = new Scanner(System.in);
 
         args = new String[3];
